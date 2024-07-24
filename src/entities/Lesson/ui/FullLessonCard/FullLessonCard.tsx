c63cc7ade8872df81127355b5bbed1e89bc4b39e
ozzzpo <onehendrix04@gmail.com>
2024-07-24 03:56:05 +0300

    cleanup for deploy

--- src/entities/Lesson/ui/FullLessonCard/FullLessonCard.tsx
+++ src/entities/Lesson/ui/FullLessonCard/FullLessonCard.tsx
@@ -1,66 +1,47 @@
-import { Lesson } from "@/shared";
-import { Box, VStack, Text} from "@chakra-ui/react";
-import styles from './FullLessonCard.module.scss'
-import { LessonTypes } from "../../constants/lessonTypes";
-import { getLessonBuilding } from "../../lib/getLessonBuilding";
-import { ArrowIcon } from "@/shared/assets/chakraIcons/ArrowIcon";
-import { sliceLessonName } from "../../lib/sliceLessonName";
-import { FullLessonDrawer } from "../FullLessonDrawer/FullLessonDrawer";
-import { useDisclosure } from "@chakra-ui/react";
-export function FullLessonCard({lesson}:{lesson:Lesson}){   
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    return(
-        <>
-        <Box
-            onClick={onOpen}
-            w='100%'
-            bgColor='#FAFAFA'
-            borderRadius='8px'
-            padding='10px 15px'
-            display='flex'
-            justifyContent='space-between'
-        >
-            <VStack 
-                alignItems='start'
-                gap='2px'
-                w='60%'
-            >
-                <Text 
-                    color='blue.900'
-                    fontWeight='bold'
-                    fontSize='18px'
-                >{sliceLessonName(lesson.discipline.name)}</Text>
-                <Text
-                    color='gray.400'
-                    fontWeight='medium'
-                    fontSize='22px'
-                >{lesson.start_time?.slice(0, 5)} {lesson.end_time && '-'}{' '} {lesson.end_time?.slice(0, 5)}</Text>
-            </VStack>
-            <VStack 
-                w='40%'
-                alignItems='center'
-                gap='0'
-            >
-                <Text
-                    fontWeight='medium'
-                    fontSize='14px'
-                >{lesson.parsed_lesson_type &&
-              LessonTypes[lesson.parsed_lesson_type]}</Text>
-                <Text
-                fontWeight='medium'
-                fontSize='14px'
-                w='60%'
-                textAlign='center'
-                >{getLessonBuilding(lesson.building_number, lesson.audience_number)}</Text>
-            </VStack>
-            <VStack
-            alignItems='center'
-            justifyContent='center'
-            >
-                <ArrowIcon transform='rotate(90deg)' color='gray.400'></ArrowIcon>
-            </VStack>
-        </Box>
-        <FullLessonDrawer lesson={lesson} isOpen={isOpen} onClose={onClose}/>
-        </>
-    )
+import { Lesson } from '@/shared';
+import { Box, VStack, Text } from '@chakra-ui/react';
+import { LessonTypes } from '../../constants/lessonTypes';
+import { getLessonBuilding } from '../../lib/getLessonBuilding';
+import { ArrowIcon } from '@/shared/assets/chakraIcons/ArrowIcon';
+import { sliceLessonName } from '../../lib/sliceLessonName';
+import { FullLessonDrawer } from '../FullLessonDrawer/FullLessonDrawer';
+import { useDisclosure } from '@chakra-ui/react';
+export function FullLessonCard({ lesson }: { lesson: Lesson }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box
+        onClick={onOpen}
+        w="100%"
+        bgColor="#FAFAFA"
+        borderRadius="8px"
+        padding="10px 15px"
+        display="flex"
+        justifyContent="space-between"
+      >
+        <VStack alignItems="start" gap="2px" w="60%">
+          <Text color="blue.900" fontWeight="bold" fontSize="18px">
+            {sliceLessonName(lesson.discipline.name)}
+          </Text>
+          <Text color="gray.400" fontWeight="medium" fontSize="22px">
+            {lesson.start_time?.slice(0, 5)} {lesson.end_time && '-'}{' '}
+            {lesson.end_time?.slice(0, 5)}
+          </Text>
+        </VStack>
+        <VStack w="40%" alignItems="center" gap="0">
+          <Text fontWeight="medium" fontSize="14px">
+            {lesson.parsed_lesson_type &&
+              LessonTypes[lesson.parsed_lesson_type]}
+          </Text>
+          <Text fontWeight="medium" fontSize="14px" w="60%" textAlign="center">
+            {getLessonBuilding(lesson.building_number, lesson.audience_number)}
+          </Text>
+        </VStack>
+        <VStack alignItems="center" justifyContent="center">
+          <ArrowIcon transform="rotate(90deg)" color="gray.400"></ArrowIcon>
+        </VStack>
+      </Box>
+      <FullLessonDrawer lesson={lesson} isOpen={isOpen} onClose={onClose} />
+    </>
+  );
 }
--- src/entities/Lesson/ui/FullLessonDrawer/FullLessonDrawer.tsx
+++ src/entities/Lesson/ui/FullLessonDrawer/FullLessonDrawer.tsx
@@ -1,153 +1,146 @@
-
 import {
-    Drawer,
-    DrawerBody,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    Text,
-    VStack,
-    Button,
-    Box,
-    Link,
-    Tabs,
-    TabList,
-    TabPanels,
-    Tab,
-    TabPanel,
-    Avatar,
+  Drawer,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Text,
+  VStack,
+  Box,
+  Link,
+  Avatar,
 } from '@chakra-ui/react';
 import { useRef } from 'react';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion';
 import { Lesson } from '@/shared';
 import { getLessonBuilding } from '../../lib/getLessonBuilding';
 import { LessonTypes } from '../../constants/lessonTypes';
 const MotionDrawerContent = motion(DrawerContent);
-export function FullLessonDrawer({  lesson,
-    isOpen,
-    onClose,}: {
-        lesson: Lesson,
-        isOpen: boolean,
-        onClose: () => void
-    }){
-
-        const parityTypes = {
-          odd: 'Нечётная неделя',
-          even: 'Чётная неделя',
-          any: 'Каждая неделя',
-        };
-    const btnRef = useRef<HTMLButtonElement>(null);
-    return(
-        <Drawer
-        isOpen={isOpen}
-        placement="bottom"
-        onClose={onClose}
-        finalFocusRef={btnRef}
+export function FullLessonDrawer({
+  lesson,
+  isOpen,
+  onClose,
+}: {
+  lesson: Lesson;
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  const parityTypes = {
+    odd: 'Нечётная неделя',
+    even: 'Чётная неделя',
+    any: 'Каждая неделя',
+  };
+  const btnRef = useRef<HTMLButtonElement>(null);
+  return (
+    <Drawer
+      isOpen={isOpen}
+      placement="bottom"
+      onClose={onClose}
+      finalFocusRef={btnRef}
+    >
+      <DrawerOverlay />
+      <MotionDrawerContent
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 0 }}
+        onDragEnd={(_, info) => {
+          if (info.offset.y > 100) {
+            onClose();
+          }
+        }}
+        minH="70vh"
+        maxH="100%"
+        borderRadius="16px 16px 0 0"
+        display="flex"
+        flex={1}
+        flexDirection="column"
+        alignItems="center"
       >
-        <DrawerOverlay />
-        <MotionDrawerContent
-          drag="y"
-          dragConstraints={{ top: 0, bottom: 0 }}
-          onDragEnd={(_, info) => {
-            if (info.offset.y > 100) {
-              onClose();
-            }
-          }}
-          minH="70vh"
-          maxH="100%"
-          borderRadius="16px 16px 0 0"
+        <DrawerCloseButton />
+        <DrawerHeader
+          w="95%"
+          padding="40px 0 0 0"
+          color="blue.900"
           display="flex"
-          flex={1}
           flexDirection="column"
-          alignItems="center"
+          gap="5px"
         >
-          <DrawerCloseButton />
-          <DrawerHeader
-            w="95%"
-            padding="40px 0 0 0"
-            color="blue.900"
+          <Text fontSize="24px" fontWeight="bold">
+            {lesson.discipline.name}
+          </Text>
+          <Text fontSize="24px" fontWeight="medium">
+            {lesson.start_time?.slice(0, -3)} {lesson.end_time && '-'}{' '}
+            {lesson.end_time?.slice(0, -3)}
+          </Text>
+          <Box
             display="flex"
-            flexDirection="column"
-            gap="5px"
+            justifyContent="space-between"
+            fontSize="16px"
+            padding="10px 0"
           >
-            <Text fontSize="24px" fontWeight="bold">
-              {lesson.discipline.name}
-            </Text>
-            <Text fontSize="24px" fontWeight="medium">
-              {lesson.start_time?.slice(0, -3)} {lesson.end_time && '-'}{' '}
-              {lesson.end_time?.slice(0, -3)}
-            </Text>
-            <Box
+            <VStack
+              w="45%"
               display="flex"
-              justifyContent="space-between"
-              fontSize="16px"
-              padding="10px 0"
+              alignItems="start"
+              gap="2px"
+              textAlign="start"
             >
-              <VStack
-                w="45%"
-                display="flex"
-                alignItems="start"
-                gap="2px"
-                textAlign="start"
-              >
-                <Text>{parityTypes[lesson.parsed_parity]}</Text>
-              </VStack>
-              <VStack
-                w="55%"
-                display="flex"
-                alignItems="end"
-                gap="2px"
-                textAlign="end"
-              >
-                <Text>
-                  {getLessonBuilding(
-                    lesson.building_number,
-                    lesson.audience_number
-                  )}
-                </Text>
-                <Text>
-                  {lesson.parsed_lesson_type &&
-                    LessonTypes[lesson.parsed_lesson_type]}
-                </Text>
-              </VStack>
-            </Box>
-            {lesson.parsed_dates && (
-              <Text fontWeight="medium" fontSize="18px">
-                Новые даты: {lesson.parsed_dates}
+              <Text>{parityTypes[lesson.parsed_parity]}</Text>
+            </VStack>
+            <VStack
+              w="55%"
+              display="flex"
+              alignItems="end"
+              gap="2px"
+              textAlign="end"
+            >
+              <Text>
+                {getLessonBuilding(
+                  lesson.building_number,
+                  lesson.audience_number
+                )}
               </Text>
-            )}
-            <Link
-              padding="10px 0"
-              fontSize="14px"
-              fontWeight="medium"
-              color="orange.300"
-              href="#"
+              <Text>
+                {lesson.parsed_lesson_type &&
+                  LessonTypes[lesson.parsed_lesson_type]}
+              </Text>
+            </VStack>
+          </Box>
+          {lesson.parsed_dates && (
+            <Text fontWeight="medium" fontSize="18px">
+              Новые даты: {lesson.parsed_dates}
+            </Text>
+          )}
+          <Link
+            padding="10px 0"
+            fontSize="14px"
+            fontWeight="medium"
+            color="orange.300"
+            href="#"
+          >
+            Сообщить об ошибке
+          </Link>
+          {lesson.teacher && (
+            <Box
+              boxShadow="0px 0px 5px 0px #00000020"
+              borderRadius="16px"
+              padding="14px"
+              display="flex"
+              alignItems="center"
+              gap="15px"
             >
-              Сообщить об ошибке
-            </Link>
-            {lesson.teacher && (
-              <Box
-                boxShadow="0px 0px 5px 0px #00000020"
-                borderRadius="16px"
-                padding="14px"
-                display="flex"
-                alignItems="center"
-                gap="15px"
-              >
-                <Avatar />
-                <Box>
-                  <Text fontSize="16px" fontWeight="medium">
-                    {lesson?.teacher?.name}
-                  </Text>
-                  <Text fontSize="12px" fontWeight="medium">
-                    {lesson.department?.name}
-                  </Text>
-                </Box>
+              <Avatar />
+              <Box>
+                <Text fontSize="16px" fontWeight="medium">
+                  {lesson?.teacher?.name}
+                </Text>
+                <Text fontSize="12px" fontWeight="medium">
+                  {lesson.department?.name}
+                </Text>
               </Box>
-            )}
-          </DrawerHeader>
-        </MotionDrawerContent>
-      </Drawer>
-    )
+            </Box>
+          )}
+        </DrawerHeader>
+      </MotionDrawerContent>
+    </Drawer>
+  );
 }
