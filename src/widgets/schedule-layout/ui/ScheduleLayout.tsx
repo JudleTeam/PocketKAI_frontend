89dbdb7b338d9f255693ae51f15f8f73ffd8e688
ozzzpo <onehendrix04@gmail.com>
2024-08-07 03:41:01 +0300

    add go up/down button for schedule

--- src/widgets/schedule-layout/ui/ScheduleLayout.tsx
+++ src/widgets/schedule-layout/ui/ScheduleLayout.tsx
@@ -3,6 +3,7 @@ import {
   Skeleton,
   Stack,
   Text,
+  useChakra,
   useColorModeValue,
 } from '@chakra-ui/react';
 import { FadedLessonCard, LessonCard, RestCard } from '@/entities';
@@ -11,11 +12,25 @@ import { useInfiniteScroll } from '../lib/useInfiniteScroll';
 //import { useCurrentDay } from '@/widgets';
 import styles from './ScheduleLayout.module.scss';
 import { getTodayDate } from '@/shared';
+import { ArrowIcon } from '@/shared/assets';
+import { useGoUpButton } from '../lib/useGoUpButton';
 export function ScheduleLayout({ schedule }: { schedule: Nullable<Schedule> }) {
   const today = getTodayDate();
   const { upperRef, lowerRef, scheduleContainerRef } = useInfiniteScroll();
+  const { showButton, position: todayBlockPosition } = useGoUpButton();
+  console.log(todayBlockPosition);
+  const { theme } = useChakra();
   const mainTextColor = useColorModeValue('light.main_text', 'dark.main_text');
-   return <div id='schedule' className={styles['schedule']} ref={scheduleContainerRef}>
+  const mainElementColor = useColorModeValue(
+    theme.colors.light.main_element,
+    theme.colors.dark.main_element
+  );
+  return (
+    <div
+      id="schedule"
+      className={styles['schedule']}
+      ref={scheduleContainerRef}
+    >
       <Stack ref={upperRef}>
         <Skeleton height="20px" />
         <Skeleton height="20px" />
@@ -66,6 +81,34 @@ export function ScheduleLayout({ schedule }: { schedule: Nullable<Schedule> }) {
         <Skeleton height="20px" />
         <Skeleton height="20px" />
       </Stack>
+
+      {showButton && (
+        <Box
+          onClick={() =>
+            document
+              .getElementById(today)
+              ?.scrollIntoView({ behavior: 'smooth' })
+          }
+          as="button"
+          w="40px"
+          h="40px"
+          borderRadius="8px"
+          position="fixed"
+          bottom="80px"
+          right="5%"
+          bgColor={mainElementColor}
+          zIndex={'50'}
+        >
+          {todayBlockPosition === 'above' ? (
+            <ArrowIcon fill={theme.colors.light.main_text} />
+          ) : (
+            <ArrowIcon
+              fill={theme.colors.light.main_text}
+              transform="rotate(180deg)"
+            />
+          )}
+        </Box>
+      )}
     </div>
-  ;
+  );
 }
