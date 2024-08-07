41843fb1a5579b06267beb0a638d241ccc95b6d0
warnoffline <hametshin543@gmail.com>
2024-08-08 01:14:10 +0300

    update icon

--- vite.config.ts
+++ vite.config.ts
@@ -25,13 +25,13 @@ export default defineConfig({
         description: 'Веб-приложение для просмотра расписания КНИТУ-КАИ',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: "maskable"
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: "maskable"
