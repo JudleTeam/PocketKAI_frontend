98d5e1ac07e752af8e3fe4bb02d188355152c15a
warnoffline <hametshin543@gmail.com>
2024-09-13 14:37:16 +0300

    add swiper in teacher drawer

--- src/entities/Teacher/ui/TeacherLessonCard.tsx
+++ src/entities/Teacher/ui/TeacherLessonCard.tsx
@@ -1,13 +1,36 @@
 import { TeacherLesson } from '@/shared';
-import { Box, VStack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  VStack,
+  Text,
+  useColorModeValue,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverHeader,
+  PopoverBody,
+} from '@chakra-ui/react';
 import { getLessonBuilding, useColor } from '@/shared/lib';
 import { useDisclosure } from '@chakra-ui/react';
 import { LessonTypes } from '@/shared/constants';
-import { ElipsisIcon } from '@/shared/assets/chakraIcons/ElipsisIcon';
 import { ProblemIcon } from '@/shared/assets';
-export function TeacherLessonCard({ lesson }: { lesson: TeacherLesson }) {
+import { InfoIcon } from '@/shared/assets/chakraIcons/InfoIcon';
+import React from 'react';
+export function TeacherLessonCard({
+  lesson,
+  openPopoverId,
+  setOpenPopoverId,
+  week,
+}: {
+  lesson: TeacherLesson;
+  openPopoverId: string | null;
+  setOpenPopoverId: (lessonId: string | null, week: 'even' | 'odd') => void;
+  week: 'even' | 'odd';
+}) {
   const { onOpen } = useDisclosure();
   const { mainTextColor } = useColor();
+  const popoverColor = useColorModeValue('gray.50', 'gray.700');
   return (
     <Box
       className="w-full"
@@ -16,13 +39,13 @@ export function TeacherLessonCard({ lesson }: { lesson: TeacherLesson }) {
       padding="8px 0"
       display="flex"
       justifyContent="space-between"
-      transition='0.2s'
-      _active={{opacity: 0.5, transition: '0.2s'}}
     >
       <VStack alignItems="start" gap="2px" w="100%">
         <Box
           color={
-            lesson.parsed_dates_status === 'need_check'
+            lesson.parsed_dates_status === 'need_check' ||
+            (lesson.parsed_dates_status === 'good' &&
+              lesson?.parsed_dates?.length > 0)
               ? '#ED8936'
               : mainTextColor
           }
@@ -33,15 +56,21 @@ export function TeacherLessonCard({ lesson }: { lesson: TeacherLesson }) {
           fontSize="16px"
           gap={'5px'}
         >
-          {lesson.parsed_dates_status === 'need_check' ?
-          <ProblemIcon alignSelf={'center'} w={'25px'} h={'25px'}></ProblemIcon>
-          : null}
-          <Text noOfLines={2}>
-          {lesson.discipline.name}
-          </Text>
+          {lesson.parsed_dates_status === 'need_check' ||
+          (lesson.parsed_dates_status === 'good' &&
+            lesson?.parsed_dates?.length > 0) ? (
+            <ProblemIcon
+              alignSelf={'center'}
+              w={'25px'}
+              h={'25px'}
+            ></ProblemIcon>
+          ) : null}
+          <Text noOfLines={2}>{lesson.discipline.name}</Text>
         </Box>
         <Text textAlign={'center'} fontSize="14px">
-          {lesson.parsed_lesson_type ? LessonTypes[lesson.parsed_lesson_type] : lesson.original_lesson_type}
+          {lesson.parsed_lesson_type
+            ? LessonTypes[lesson.parsed_lesson_type]
+            : lesson.original_lesson_type}
         </Text>
         <Text color="gray.400" fontWeight="medium" fontSize="20px">
           {lesson.start_time?.slice(0, 5)} {lesson.end_time && '-'}{' '}
@@ -59,8 +88,61 @@ export function TeacherLessonCard({ lesson }: { lesson: TeacherLesson }) {
           {getLessonBuilding(lesson.building_number, lesson.audience_number)}
         </Text>
       </VStack>
-      <VStack alignItems='center' justifyContent='center'>
-        <ElipsisIcon w='25px' h='25px' color={mainTextColor}/> 
+      <VStack alignItems="center" px={'5px'} justifyContent="center">
+        <Popover
+          isLazy
+          key={lesson.id}
+          isOpen={openPopoverId === lesson.id}
+          onOpen={() => setOpenPopoverId(lesson.id, week)}
+          onClose={() => setOpenPopoverId(null, week)}
+        >
+          <PopoverTrigger>
+            <InfoIcon
+              transition="0.2s"
+              _active={{ opacity: 0.5, transition: '0.2s' }}
+              w={'25px'}
+              h={'25px'}
+            />
+          </PopoverTrigger>
+          <PopoverContent bgColor={popoverColor}>
+            <PopoverArrow bg={popoverColor} />
+            <PopoverHeader
+              fontSize="16px"
+              fontWeight={'bold'}
+              color={mainTextColor}
+            >
+              {lesson.discipline.name}
+            </PopoverHeader>
+            <PopoverBody
+              fontSize={'16px'}
+              fontWeight={'medium'}
+              color={mainTextColor}
+              display="flex"
+              flexDirection={'column'}
+              justifyContent={'start'}
+              alignItems={'start'}
+              gap="5px"
+            >
+              <Box>
+                {lesson.parsed_dates_status === 'need_check' ||
+                (lesson.parsed_dates_status === 'good' &&
+                  lesson?.parsed_dates?.length > 0) ? (
+                  <Text>Даты проведения: {lesson.original_dates}</Text>
+                ) : null}
+              </Box>
+              <Box display="flex" flexWrap={'wrap'}>
+                <Text>Группы:&nbsp;</Text>
+                {lesson.groups.map((group, index) => (
+                  <React.Fragment key={group.id}>
+                    {`${group.group_name}${
+                      lesson.groups.length - 1 === index ? '' : ', '
+                    }`}
+                  </React.Fragment>
+                ))}
+              </Box>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </VStack>
     </Box>
   );
