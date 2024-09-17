9334236f70eb6e04c22ac93e3d92cf982854fd2e
warnoffline <hametshin543@gmail.com>
2024-09-17 18:56:56 +0300

    fix statusbar

--- src/shared/ui/idle-message/IdleMessage.tsx
+++ src/shared/ui/idle-message/IdleMessage.tsx
@@ -22,7 +22,7 @@ export function IdleMessage() {
       if (isOpen){
         metaThemeColor.setAttribute('content', modalColor)
       }
-      else {
+      if(!drawer.isOpen && !isOpen) {
         metaThemeColor.setAttribute('content', mainColor);
       }
     }
