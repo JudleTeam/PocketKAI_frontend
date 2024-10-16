174c052f2a62c81bf0410f199b5e4ae235d418c8
warnoffline <hametshin543@gmail.com>
2024-10-16 15:09:46 +0300

    fix number of days

--- src/entities/Schedule/lib/isScheduleOutdated.ts
+++ src/entities/Schedule/lib/isScheduleOutdated.ts
@@ -6,5 +6,5 @@ export const isScheduleOutdated = (parsedAt: string) => {
   const scheduleParsedDate =
     DateTime.fromISO(parsedAt).setZone('Europe/Moscow');
   const todayDate = DateTime.fromISO(today).setZone('Europe/Moscow');
-  return todayDate.diff(scheduleParsedDate, 'days').days > 7;
+  return todayDate.diff(scheduleParsedDate, 'days').days > 3;
 };
--- src/widgets/app-layout/lib/isScheduleOutdatedInternet.tsx
+++ src/widgets/app-layout/lib/isScheduleOutdatedInternet.tsx
@@ -3,12 +3,12 @@ import { DateTime } from 'luxon';
 
 export const isScheduleOutdatedInternet = () => {
     const parsedAt = localStorage.getItem('cacheUpdateDate') 
-  const today = getTodayDate();
-  if(parsedAt){
-    const scheduleParsedDate =
-        DateTime.fromISO(parsedAt).setZone('Europe/Moscow');
-    const todayDate = DateTime.fromISO(today).setZone('Europe/Moscow');
-    return todayDate.diff(scheduleParsedDate, 'days').days > 3;
-  }
-  return true;
+    const today = getTodayDate();
+    if(parsedAt){
+        const scheduleParsedDate =
+            DateTime.fromISO(parsedAt).setZone('Europe/Moscow');
+        const todayDate = DateTime.fromISO(today).setZone('Europe/Moscow');
+        return todayDate.diff(scheduleParsedDate, 'days').days > 3;
+    }
+    return true;
 };
